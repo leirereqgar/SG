abb173add5981b3cfdd96e672444758e6a99631d
Leire Requena Garcia <leirereqgar@gmail.com>
2021-04-25 17:04:05 +0200

    Ya funciona el barrido por fin

--- Práctica 1/ejerciciosThree/ejercicio_4/barrido.js
+++ Práctica 1/ejerciciosThree/ejercicio_4/barrido.js
@@ -4,39 +4,54 @@ class Barrido extends THREE.Object3D {
 	constructor() {
 		super();
 
-		var contorno = new THREE.Shape();
-		contorno.moveTo(0, 4.75);
-		contorno.lineTo(2.375, 0);
-		contorno.lineTo(0, -4.75);
-		contorno.lineTo(-2.375, 0);
-		contorno.lineTo(0, 4.75);
+		var shape = this.createShape();
 
-		var vector_recorrido = [];
-		vector_recorrido.push(new THREE.Vector3(0.0, 0.0, 0.0));
-		vector_recorrido.push(new THREE.Vector3(0.0, 0.0, 5.0));
-		vector_recorrido.push(new THREE.Vector3(0.0, 10.0, 10.0));
-		vector_recorrido.push(new THREE.Vector3(-10.0, 0.0, 15.0));
+		var path = new THREE.CatmullRomCurve3([
+			new THREE.Vector3( -20, 5, 0 ),
+			new THREE.Vector3( -15, 5, 15 ),
+			new THREE.Vector3( -10, 0, 10 ),
+			new THREE.Vector3( -5, 5, 5 ),
+			new THREE.Vector3( 0, 0, 0 ),
+			new THREE.Vector3( 5, -5, 5 ),
+			new THREE.Vector3( 10, 0, 10 ),
+			new THREE.Vector3( 15, 5, 15 ),
+			new THREE.Vector3( 20, 5, 0 )
+		]);
 
-		var recorrido = new THREE.CatmullRomCurve3(vector_recorrido);
-
-		const opciones = {
-			steps: 1,
-			depth: 1,
-			extrudePath: recorrido
+		var options = {
+			steps: 50, curveSegments: 20, extrudePath: path
 		};
 
-		var geometria = new THREE.ExtrudeGeometry(contorno, opciones);
-		var material  = new THREE.MeshNormalMaterial();
+		var geometry = new THREE.ExtrudeGeometry( shape, options );
 
-		this.objeto   = new THREE.Mesh(geometria, opciones);
+		var material = new THREE.MeshPhongMaterial( { color: 0xff0000 } );
 
-		this.add(this.objeto);
+		this.columna = new THREE.Mesh( geometry, material ) ;
+
+		this.columna.scale.set(0.6,0.3,0.3);
+		this.rotation.z = Math.PI/2;
+
+		this.add( this.columna);
+	}
+
+	createShape(){
+		var shape = new THREE.Shape();
+
+		var x = -2.5;
+		var y = -4.0;
+		shape.moveTo(x + 2.5, y + 2.5);
+		shape.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
+		shape.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
+		shape.bezierCurveTo(x - 3, y + 5.5, x - 1.5, y + 7.7, x + 2.5, y + 9.5);
+		shape.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
+		shape.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
+		shape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
+
+		return shape;
 	}
 
 	update () {
-		this.objeto.rotation.x += 0.02;
-		this.objeto.rotation.y += 0.02;
-		this.objeto.rotation.z += 0.02;
+		this.rotation.y += 0.02;
 	}
 }
 
