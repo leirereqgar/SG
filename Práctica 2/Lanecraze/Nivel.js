feb5f21060c7f47c07fe8d2c4155383dc616f44f
clarasdfgh <claramariaromerolara@gmail.com>
2021-06-07 08:43:00 +0200

    arboles centrados

--- Práctica 2/Lanecraze/Nivel.js
+++ Práctica 2/Lanecraze/Nivel.js
@@ -14,10 +14,6 @@ class Nivel extends THREE.Object3D {
 	constructor() {
 		super();
 
-		const block = 15;
-		const ancho = 20;
-		const largo = 60;
-
 		//Vector de generación para el suelo del tablero
 		var v_gen = new Array(7);
 		v_gen[0] = new THREE.Vector2(5,0);
@@ -31,6 +27,10 @@ class Nivel extends THREE.Object3D {
 		this.suelo = new Suelo(v_gen);
 		this.suelo.position.set(0,0, -this.suelo.getLargo()/2);
 		this.add(this.suelo);
+
+		this.block = 15;
+		this.ancho = this.suelo.getAncho()/15;
+		this.largo = this.suelo.getLargo()/15;
 
 		/*var arbol = new Arbol(2);
 		arbol.position.set(-30+15/2,0,-30);
@@ -59,38 +59,46 @@ class Nivel extends THREE.Object3D {
 	}
 
 	posicionAleatoriaX(){
-		const ancho = 20;
-		const block = 15;
 		/*Posicionar:
-			1.Tomamos el ancho del tablero y lo dividimos entre 2 (ya que una mitad es x positiva y la otra x negativa)
-			2.Generamos un número aleatorio del 0 al 10 y hacemos módulo del número anterior. Esta será la "casilla" de x
-			3.Para traducir la casilla a una posición, lo trasladamos multiplicando por el tamaño de bloque entre 2 (el centro)
-			4.Finalmente, aleatorizamos si la posición será positiva o negativa con el operador terciario
+			1.Tomamos el ancho del tablero y lo dividimos en mitades
+			2.Generamos un número aleatorio del entre -ancho_bloques y +ancho_bloques
+			3.Para traducir el num_aleatorio a una posición, lo trasladamos multiplicando por el tamaño de bloque,
+				y luego le quitamos medio bloque para dejarlo centrado
 		*/
+		var ancho_bloques = (this.ancho/2) - 1;
 
-		var ancho_posible = (ancho / 2) - 1;
-		var casilla  = (Math.random() * 10) % ancho_posible;
-		var posicion = (casilla * block/2 ) * (Math.random() < 0.5 ? -1 : 1);
+		var num_aleatorio = Math.round(Math.random() * (ancho_bloques - (ancho_bloques*-1)) + (ancho_bloques*-1));
+		console.log(num_aleatorio);
+		var posicion = num_aleatorio * (this.block) - (this.block/2);
+
+		/*if(num_aleatorio == -5){
+			posicion += this.block;
+		}
+
+		if(num_aleatorio == 5){
+			posicion -= this.block;
+		}*/
 
 		return posicion;
 	}
 
 	generaArboles(){
-		const block = 15;
-		const ancho = 20;
-		const largo = 60;
-
 		this.arboles = [];
 		this.arbustos = [];
 
 		this.num_arboles = 0;
 		this.num_arbustos = 0;
 
-		for(var i=3; i<largo; i++){
-			if(this.suelo.getTipo() == 0){
-				//Se define el número de obstáculos permitidos en cada fila: un 5% de las casillas de la fila, mas o menos 1.
+
+		var tablero = [];
+		tablero = this.suelo.getTableroVirtual();
+
+
+		for(var i=2; i<this.largo; i++){
+			if(tablero[i].getTipo() == 0){
+				//Se define el número de obstáculos permitidos en cada fila: un 10% de las casillas de la fila, mas o menos 1.
 				//El | 0 se añade para que quede un número entero.
-				var num_obstaculos = ((ancho * 0.05) | 0) + (Math.random() < 0.5 ? -1 : 1);
+				var num_obstaculos = ((this.ancho * 0.1) | 0) + (Math.random() < 0.5 ? 0 : 1);
 
 				for(var j=0; j<num_obstaculos ; j++){
 
@@ -100,26 +108,20 @@ class Nivel extends THREE.Object3D {
 						this.arboles[this.num_arboles] = new Arbol(2);
 
 						var posicion = this.posicionAleatoriaX();
-						this.arboles[this.num_arboles].position.set(posicion, 0,-i*block/2);
+						this.arboles[this.num_arboles].position.set(posicion, 0,-i*(this.block));
 
+						this.add(this.arboles[this.num_arboles]);
 						this.num_arboles++;
 
 					} else{
 						this.arbustos[this.num_arbustos] = new Arbol(1);
 
 						var posicion = this.posicionAleatoriaX();
-						this.arbustos[this.num_arbustos].position.set(posicion, 0,-i*block/2);
+						this.arbustos[this.num_arbustos].position.set(posicion, 0,-i*this.block);
 
+						this.add(this.arbustos[this.num_arbustos]);
 						this.num_arbustos++;
 					}
-				}
-
-				for(var k=0; k<this.num_arboles; k++ ){
-					this.add(this.arboles[k]);
-				}
-
-				for(var l=0; l<this.num_arbustos; l++ ){
-					this.add(this.arbustos[l]);
 				}
 			}
 		}
@@ -133,11 +135,11 @@ class Nivel extends THREE.Object3D {
 		return this.suelo.inBounds(coord);
 	}
 
-	isWater(coord) {
+	/*isWater(coord) {
 		var tablero = this.suelo.getTableroVirtual();
 
 		return (tablero[Math.abs(Math.round(coord)/15)].getTipo() == 2);
-	}
+	}*/
 
 	intersect(nueva_pos){
 		var colision = false;
